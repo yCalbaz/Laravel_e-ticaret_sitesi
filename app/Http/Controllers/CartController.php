06b8b458a7e828d4b299eca5931e4a486d462bcc
yCalbaz <yasemincalbaz02@gmail.com>
2025-03-07 10:25:35 +0300

    -1,-2 olayları çözüldü

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -73,139 +73,138 @@ class CartController extends Controller
     }
 
     public function approvl(Request $request)
-{
-    if ($request->isMethod('post')) {
-        $cartItems = Cart::all();
-        $totalPrice = 0;
-        $stokError = false;
-        $storeId = [];
+    {
+        if ($request->isMethod('post')) {
+            $cartItems = Cart::all();
+            $totalPrice = 0;
+            $stokError = false;
+            $storeId = [];
 
-        foreach ($cartItems as $item) {
-            $response = Http::get("http://host.docker.internal:3000/stock/{$item->product_sku}");
+            foreach ($cartItems as $item) {
+                $response = Http::get("http://host.docker.internal:3000/stock/{$item->product_sku}");
 
-            if ($response->failed()) {
-                return redirect()->with('error', 'Stok servisine ulaşılmıyor');
-            }
-
-            $stockData = $response->json();
-            if (!isset($stockData['stores'])) {
-                return redirect()->back()->with('error', 'Stok verisi alınamadı');
-            }
-
-            $totalStock = 0;
-            foreach ($stockData['stores'] as $store) {
-                $totalStock += $store['stock'];
-               
-                $storeId[$item->product_sku] = $store['store_id'];  
-            }
-
-            if ($totalStock < $item->product_piece) {
-                $stokError = true;
-                break;
-            }
-
-            $totalPrice += ($item->product_price * $item->product_piece);
-        }
-
-        if ($stokError) {
-            return redirect()->back()->with('error', 'Yeterli stok yok');
-        }
-
-        $adSoyad = $request->input('adSoyad');
-        $adres = $request->input('adres');
-
-        $orderBatch = OrderBatch::create([
-            'customer_name' => $adSoyad,
-            'customer_address' => $adres,
-            'product_price' => $totalPrice,
-        ]);
-
-        $orderId = $orderBatch->id;
-        $orderBatch->order_id = $orderId;
-        $orderBatch->save();
-
-        $groupedItems = [];
-        foreach ($cartItems as $item) {
-            $store = $storeId[$item->product_sku];  
-            if (!isset($groupedItems[$store])) {
-                $groupedItems[$store] = [];
-            }
-            $groupedItems[$store][] = $item;
-        }
-
-        $subOrderId = 1;
-        foreach ($groupedItems as $store => $items) {
-            $orderLinesData = [];
-            foreach ($items as $item) {
-                for ($i = 0; $i < $item->product_piece; $i++) {
-                    $orderLinesData[] = [
-                        'product_sku' => $item->product_sku,
-                        'product_name' => $item->product_name,
-                        'store_id' => $store,
-                        'order_id' => $orderId . '-' . $subOrderId,
-                        'order_batch_id' => $orderId,
-                    ];
+                if ($response->failed()) {
+                    return redirect()->with('error', 'Stok servisine ulaşılmıyor');
                 }
+
+                $stockData = $response->json();
+                if (!isset($stockData['stores'])) {
+                    return redirect()->back()->with('error', 'Stok verisi alınamadı');
+                }
+
+                $totalStock = 0;
+                foreach ($stockData['stores'] as $store) {
+                    $totalStock += $store['stock'];
+
+                    $storeId[$item->product_sku] = $store['store_id'];
+                }
+
+                if ($totalStock < $item->product_piece) {
+                    $stokError = true;
+                    break;
+                }
+
+                $totalPrice += ($item->product_price * $item->product_piece);
             }
-            try {
-                OrderLine::insert($orderLinesData);
-            } catch (\Exception $e) {
-                return redirect()->back()->with('error', 'Sipariş oluşturulurken hata oludu.' . $e->getMessage());
+
+            if ($stokError) {
+                return redirect()->back()->with('error', 'Yeterli stok yok');
             }
-            $subOrderId++;
-        }
 
-        foreach ($groupedItems as $store => $items) {
-            foreach ($items as $item) {
-                Log::info("Stok Güncelleme İşlemi Başlatılıyor: " . json_encode([
-                    'product_sku' => $item->product_sku,
-                    'product_piece' => $item->product_piece,
-                    'store_id' => $store,
-                ]));
+            $adSoyad = $request->input('adSoyad');
+            $adres = $request->input('adres');
 
-                // Stok kontrolü
-                $currentStock = DB::table('stocks')
-                    ->where('product_sku', $item->product_sku)
-                    ->where('store_id', $store)
-                    ->value('product_piece');
+            $orderBatch = OrderBatch::create([
+                'customer_name' => $adSoyad,
+                'customer_address' => $adres,
+                'product_price' => $totalPrice,
+            ]);
 
-                if ($currentStock < $item->product_piece) {
-                    Log::error("Stok Yetersiz! " . json_encode([
+            $orderId = $orderBatch->id;
+            $orderBatch->order_id = $orderId;
+            $orderBatch->save();
+
+            $groupedItems = [];
+            foreach ($cartItems as $item) {
+                $store = $storeId[$item->product_sku];
+                if (!isset($groupedItems[$store])) {
+                    $groupedItems[$store] = [];
+                }
+                $groupedItems[$store][] = $item;
+            }
+
+            $subOrderId = 1;
+            foreach ($groupedItems as $store => $items) {
+                $orderLinesData = [];
+                foreach ($items as $item) {
+                    for ($i = 0; $i < $item->product_piece; $i++) {
+                        $orderLinesData[] = [
+                            'product_sku' => $item->product_sku,
+                            'product_name' => $item->product_name,
+                            'store_id' => $store,
+                            'order_id' => (count($groupedItems) > 1) ? $orderId . '-' . $subOrderId : $orderId,
+                            'order_batch_id' => $orderId,
+                        ];
+                    }
+                }
+                try {
+                    OrderLine::insert($orderLinesData);
+                } catch (\Exception $e) {
+                    return redirect()->back()->with('error', 'Sipariş oluşturulurken hata oludu.' . $e->getMessage());
+                }
+                $subOrderId++;
+            }
+
+            foreach ($groupedItems as $store => $items) {
+                foreach ($items as $item) {
+                    Log::info("Stok Güncelleme İşlemi Başlatılıyor: " . json_encode([
                         'product_sku' => $item->product_sku,
+                        'product_piece' => $item->product_piece,
                         'store_id' => $store,
-                        'current_stock' => $currentStock,
-                        'requested_stock' => $item->product_piece,
                     ]));
 
-                    return redirect()->back()->with('error', 'Yeterli stok bulunmamaktadır!');
+                    // Stok kontrolü
+                    $currentStock = DB::table('stocks')
+                        ->where('product_sku', $item->product_sku)
+                        ->where('store_id', $store)
+                        ->value('product_piece');
+
+                    if ($currentStock < $item->product_piece) {
+                        Log::error("Stok Yetersiz! " . json_encode([
+                            'product_sku' => $item->product_sku,
+                            'store_id' => $store,
+                            'current_stock' => $currentStock,
+                            'requested_stock' => $item->product_piece,
+                        ]));
+
+                        return redirect()->back()->with('error', 'Yeterli stok bulunmamaktadır!');
+                    }
+
+                    // Stok düşme işlemi
+                    DB::table('stocks')
+                        ->where('product_sku', $item->product_sku)
+                        ->where('store_id', $store)
+                        ->decrement('product_piece', $item->product_piece);
+
+                    Log::info("Stok Güncellendi: " . json_encode([
+                        'product_sku' => $item->product_sku,
+                        'store_id' => $store,
+                        'used_stock' => $item->product_piece,
+                        'remaining_stock' => $currentStock - $item->product_piece
+                    ]));
                 }
-
-                // Stok düşme işlemi
-                DB::table('stocks')
-                    ->where('product_sku', $item->product_sku)
-                    ->where('store_id', $store)
-                    ->decrement('product_piece', $item->product_piece);
-
-                Log::info("Stok Güncellendi: " . json_encode([
-                    'product_sku' => $item->product_sku,
-                    'store_id' => $store,
-                    'used_stock' => $item->product_piece,
-                    'remaining_stock' => $currentStock - $item->product_piece
-                ]));
             }
+
+            Cart::truncate();
+            return redirect()->route('sepet.approvl')->with('success', 'Sipariş onaylandı');
         }
 
-        Cart::truncate();
-        return redirect()->route('sepet.approvl')->with('success', 'Sipariş onaylandı');
+        $cartItems = Cart::all();
+        $totalPrice = 0;
+        foreach ($cartItems as $item) {
+            $totalPrice += ($item->product_price * $item->product_piece);
+        }
+        $data = compact('cartItems', 'totalPrice');
+        return view('sepet_onay', $data);
     }
-
-    $cartItems = Cart::all();
-    $totalPrice = 0;
-    foreach ($cartItems as $item) {
-        $totalPrice += ($item->product_price * $item->product_piece);
-    }
-    $data = compact('cartItems', 'totalPrice');
-    return view('sepet_onay', $data);
-}
-
 }
